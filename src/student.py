c774f35917825d3619593af9d5b0e6dae0a4919f
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-08-28 08:42:28 +0200

    init student

--- src/student.py
+++ src/student.py
@@ -1,7 +1,13 @@
+from src.address import Address
+
 from src.user import User
 
 
 class Student(User):
-   def __init__(self,firstName,lastName,age,adress):
-       super().__init__()
-       isConnected = True
+    def __init__(self, first_name, last_name, age, address: Address):
+        super().__init__()
+        self.isConnected = True
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.address = address

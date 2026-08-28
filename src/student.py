9410dd0fe3c2ecc28d87e0635855f1d3b936bee6
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-28 09:14:24 +0200

    fix: Add id in Student class.

--- src/student.py
+++ src/student.py
@@ -1,22 +1,19 @@
 from src.address import Address
-
 from src.user import User
 import itertools
 
 
-class Resource:
-    _id_counter = itertools.count(1)  # Start from 1
-
-
 class Student(User):
+    __id_counter = itertools.count(0)  # Start from 0
+
     def __init__(self, first_name, last_name, age, address: Address):
         super().__init__()
+        self.id = next(Student.__id_counter)
         self.isConnected = True
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.address = address
-        self.id = next(Resource._id_counter)
 
     def update_account(self, address: Address):
         self.address = address

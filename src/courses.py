e2dda7bb35b4afcba3fcaf5fe435e7ed60924464
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-28 13:31:02 +0200

    refactor: Recompute Director class.

--- src/courses.py
+++ src/courses.py
@@ -11,8 +11,10 @@ class Courses:
     def add_course(self, name: str, start_date: date, end_date: date, teacher: Teacher):
         self.list_courses.append(Course(name, start_date, end_date, teacher))
 
-    def remove_course(self, index: int):
-        if 0 <= index < len(self.list_courses):
-            self.list_courses.pop(index)
+    def remove_course(self, course: Course):
+        index = self.list_courses.index(course)
+
+        if index.is_integer():
+            self.list_courses.remove(course)
         else:
-            print("L'index pointe en dehors du tableau des cours.")
+            print("Le cours n'est pas dans le tableau des cours.")
--- src/director.py
+++ src/director.py
@@ -1,4 +1,5 @@
 from src.courses import Courses
+from src.course import Course
 from src.teacher import Teacher
 from src.secretary import Secretary
 
@@ -6,11 +7,10 @@ from src.secretary import Secretary
 class Director(Teacher, Secretary):
     def __init__(self):
         super().__init__()
-        self.courses = []
+        self.courses = Courses()
 
-    def create_course(self, course: Courses):
-        self.courses.append(course)
+    def create_course(self, course: Course):
+        self.courses.add_course(course)
 
-    def delete_course(self, course: Courses):
-        if course in self.courses:
-            self.courses.remove(course)
+    def delete_course(self, course: Course):
+        self.courses.remove_course(course)

7dc65a9d9df02d869ebb7c8b15bfaadc2f2c6e1b
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-08-28 09:26:55 +0200

    changement de age en date_of_birth

--- src/student.py
+++ src/student.py
@@ -1,18 +1,19 @@
 from src.address import Address
 from src.user import User
 import itertools
+from xmlrpc.client import DateTime
 
 
 class Student(User):
     __id_counter = itertools.count(0)  # Start from 0
 
-    def __init__(self, first_name, last_name, age, address: Address):
+    def __init__(self, first_name, last_name, date_of_birth: DateTime, address: Address):
         super().__init__()
         self.id = next(Student.__id_counter)
         self.isConnected = True
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
+        self.date_of_birth = date_of_birth
         self.address = address
 
     def update_account(self, address: Address):
--- src/teacher.py
+++ src/teacher.py
@@ -1,16 +1,17 @@
 from src.user import User
 from src.address import Address
+from xmlrpc.client import DateTime
+
 
 
 class Teacher(User):
-    def __init__(self, first_name, last_name, age, address: Address):
+    def __init__(self, first_name, last_name, date_of_birth: DateTime, address: Address):
         super().__init__()
         self.isConnected = True
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
+        self.date_of_birth = date_of_birth
         self.address = address
 
     def update_account(self, address: Address):
         self.address = address
-    

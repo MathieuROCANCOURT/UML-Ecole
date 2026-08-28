3df385413f9984bc51e9788e0571ac9cc1616bbd
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-08-28 10:09:58 +0200

    remove course in teacher

--- src/teacher.py
+++ src/teacher.py
@@ -5,7 +5,7 @@ from src.courses import Courses
 
 
 class Teacher(User):
-    def __init__(self, first_name, last_name, date_of_birth: DateTime, address: Address, course: Courses,
+    def __init__(self, first_name, last_name, date_of_birth: DateTime, address: Address,
                  arrival_date: DateTime):
         super().__init__()
         self.isConnected = True
@@ -13,7 +13,6 @@ class Teacher(User):
         self.last_name = last_name
         self.date_of_birth = date_of_birth
         self.address = address
-        self.course = course
         self.arrival_date = arrival_date
 
     def update_account(self, address: Address):

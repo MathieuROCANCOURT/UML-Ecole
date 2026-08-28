84d5ea23130a5c1a3863c40672cd98dc81f34e45
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-08-28 09:20:12 +0200

    creation de teacher

---
+++ src/teacher.py
@@ -0,0 +1,16 @@
+from src.user import User
+from src.address import Address
+
+
+class Teacher(User):
+    def __init__(self, first_name, last_name, age, address: Address):
+        super().__init__()
+        self.isConnected = True
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.address = address
+
+    def update_account(self, address: Address):
+        self.address = address
+    

5324fefb0bf159f4be59d9e137a4375bc32cbc4c
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-08-28 09:50:28 +0200

    arrival_date & course in teacher

--- src/teacher.py
+++ src/teacher.py
@@ -1,16 +1,20 @@
 from src.user import User
 from src.address import Address
 from xmlrpc.client import DateTime
+from src.courses import Courses
 
 
 class Teacher(User):
-    def __init__(self, first_name, last_name, date_of_birth: DateTime, address: Address):
+    def __init__(self, first_name, last_name, date_of_birth: DateTime, address: Address, course: Courses,
+                 arrival_date: DateTime):
         super().__init__()
         self.isConnected = True
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
         self.address = address
+        self.course = course
+        self.arrival_date = arrival_date
 
     def update_account(self, address: Address):
         self.address = address

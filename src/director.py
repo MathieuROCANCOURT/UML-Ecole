f3a1fb79cfee9c657d3579b017def260dd6fb03e
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-08-28 11:30:09 +0200

    ajout & suppression de course

--- src/director.py
+++ src/director.py
@@ -1,3 +1,4 @@
+from src.courses import Courses
 from src.teacher import Teacher
 from src.secretary import Secretary
 
@@ -5,5 +6,11 @@ from src.secretary import Secretary
 class Director(Teacher, Secretary):
     def __init__(self):
         super().__init__()
+        self.courses = []
 
-        
+    def create_course(self, course: Courses):
+        self.courses.append(course)
+
+    def delete_course(self, course: Courses):
+        if course in self.courses:
+            self.courses.remove(course)
